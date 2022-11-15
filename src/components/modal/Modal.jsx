4c37c19fa31c23175631b1bb558726b20b9481a0
Victoria <adedayovicky123@gmail.com>
2022-11-15 15:34:49 +0100

    :sparkles: add mobile hamburger navigation

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -15,17 +15,17 @@ const Modal = ({ open, onClose }) => {
         onClick={(e) => {
           e.stopPropagation();
         }}
-        className="flex flex-col basis-1/3 white_bg rounded-2xl py-10 px-5 mt-20"
+        className="flex flex-col basis-5/12 white_bg rounded-2xl py-10 mt-20"
       >
-        <div className="flex justify-between">
+        <div className="flex justify-between px-8">
           <h1 className="text-darkGray font-semibold text-2xl">
             Connect Wallet
           </h1>
           <img onClick={onClose} src={x} alt="close" />
         </div>{' '}
         <p className="border border-line my-8"></p>
-        <p className="mb-4 text-base">Choose your preferred wallet:</p>
-        <div className="flex justify-between items-center border border-line bg-lightMilk py-3 px-5 mb-4">
+        <p className="mb-4 text-base px-8">Choose your preferred wallet:</p>
+        <div className="flex justify-between items-center border border-line rounded-xl bg-lightMilk py-3 mx-8 px-5 mb-4">
           <div className="flex items-center gap-4">
             <img src={metaImg} alt="Meta Mask" />
             <h2 className="text-black font-semibold text-lg">Metamask</h2>
@@ -34,7 +34,7 @@ const Modal = ({ open, onClose }) => {
             <img src={backIcon} alt="See more icon" />
           </div>
         </div>
-        <div className="flex justify-between items-center border border-line bg-lightMilk py-3 px-5">
+        <div className="flex justify-between items-center border border-line rounded-xl bg-lightMilk py-3 mx-8 px-5">
           <div className="flex items-center gap-4">
             <img src={walletImg} alt="Meta Mask" />
             <h2 className="text-black font-bold text-lg">WalletConnect</h2>
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,111 +5,83 @@ import logo from './assets/logo.svg';
 
 const Nav = () => {
   const [openModal, setOpenModal] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
   return (
     <>
-      {/*desktop navigation */}
-      <nav className="fixed top-0 white_bg w-full z-10 md:!hidden xxl:flex xxl:justify-between xxl:items-center xxl:py-4 xxl:px-24 lg:!px-10">
-        <img src={logo} alt="metabnb" className="w-40" />
-        <div className="mb-4"></div>
-        <ul className="xxl:flex xxl:gap-x-12">
-          <li>
-            <Link
-              to="/"
-              href="https://github.com/VictoriaAde/metabnb"
-              className="xxl:text-xl text-gray"
+      <nav
+        onClick={() => setIsMobile(false)}
+        className="fixed top-0 white_bg w-full z-10 md:flex xxl:justify-between xxl:items-center xxl:py-4 xxl:px-24 lg:!px-10"
+      >
+        <img src={logo} alt="metabnb" className="w-40 xxl:hidden md:!block" />
+
+        <div className={isMobile ? 'mobileNav' : 'desktopNav'}>
+          <div>
+            <img
+              src={logo}
+              alt="metabnb"
+              className="w-40 xxl:block md:!hidden"
+            />
+          </div>
+          <div>
+            <ul className="xxl:flex md:flex-col xxl:gap-x-12">
+              <li>
+                <Link
+                  to="/"
+                  href="https://github.com/VictoriaAde/metabnb"
+                  className="xxl:text-xl text-gray"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={`/Place-to-stay`}
+                  href="https://github.com/VictoriaAde/metabnb"
+                  className="text-xl text-gray"
+                >
+                  Place to stay
+                </Link>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/VictoriaAde/metabnb"
+                  className="text-xl text-gray"
+                >
+                  NFTs
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/VictoriaAde/metabnb"
+                  className="text-xl text-gray"
+                >
+                  Community
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <button
+              onClick={() => setOpenModal(!openModal)}
+              className="purple_bg text-white py-3 xxl:px-5 rounded-xl"
             >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              to={`/Place-to-stay`}
-              href="https://github.com/VictoriaAde/metabnb"
-              className="text-xl text-gray"
-            >
-              Place to stay
-            </Link>
-          </li>
-          <li>
-            <a
-              href="https://github.com/VictoriaAde/metabnb"
-              className="text-xl text-gray"
-            >
-              NFTs
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/VictoriaAde/metabnb"
-              className="text-xl text-gray"
-            >
-              Community
-            </a>
-          </li>
-        </ul>
+              Connect wallet
+            </button>
+          </div>
+        </div>
+
         <button
-          onClick={() => setOpenModal(true)}
-          className="purple_bg text-white py-3 xxl:px-5 rounded-xl"
+          onClick={() => setIsMobile(!isMobile)}
+          className="hamburger-btn xxl:hidden md:!block"
         >
-          Connect wallet
+          {isMobile ? (
+            <i className="fas fa-times"></i>
+          ) : (
+            <i className="fas fa-bars"></i>
+          )}
         </button>
         <Modal open={openModal} onClose={() => setOpenModal(false)} />
-      </nav>
-      {/* mobile navigation */}
-      <nav className=" mobile-menu fixed top-0 white_bg w-full z-10 xxl:hidden md:!flex justify-between md:!py-5 md:!px-8">
-        <div className="">
-          <img src={logo} alt="metabnb" className="w-28" />
-        </div>
-
-        <button className="menu_btn" id="menuBtn">
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </button>
-        <div className="mobile-nav hidden">
-          <ul className="md:flex-col md:gap-y-7">
-            <li>
-              <Link
-                to="/"
-                href="https://github.com/VictoriaAde/metabnb"
-                className="xxl:text-xl sm:text-xs text-gray"
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to={`/Place-to-stay`}
-                href="https://github.com/VictoriaAde/metabnb"
-                className="text-xl text-gray"
-              >
-                Place to stay
-              </Link>
-            </li>
-            <li>
-              <a
-                href="https://github.com/VictoriaAde/metabnb"
-                className="text-xl text-gray"
-              >
-                NFTs
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://github.com/VictoriaAde/metabnb"
-                className="text-xl text-gray"
-              >
-                Community
-              </a>
-            </li>
-          </ul>
-
-          <button className="purple_bg text-white mt-5 py-3 w-full rounded-xl">
-            Connect wallet
-          </button>
-        </div>
       </nav>
     </>
   );
